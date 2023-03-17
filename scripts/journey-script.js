453f2b43fb9f9c5a840e78b76dc589e6a5454e76
Duygu Temur <duygu@frank.com.tr>
2023-03-17 12:43:29 +0300

    add journey card

--- scripts/journey-script.js
+++ scripts/journey-script.js
@@ -1,104 +1,131 @@
 try {
-    
   const locations = document.getElementById("locations");
   const destinations = document.getElementById("destinations");
-  const journeyOrigin = document.getElementById("journeyOrigin");
-  const journeyDestination = document.getElementById("journeyDestination");
-  const departureTime = document.getElementById("departureTime");
-  const arrivalTime = document.getElementById("arrivalTime");
   const journeyDate = document.getElementById("journeyDate");
-  const price = document.getElementById("price");
-  const currency = document.getElementById("currency");
+  const cardWrapper = document.querySelectorAll(".card-wrapper");
 
-const getJourneys = () => {
- 
-
-
-  const lastSelectedOptionOrigin = localStorage.getItem("selectedOptionOrigin");
-  const lastSelectedOptionDestination = localStorage.getItem(
+  const getJourneys = () => {
+    const lastSelectedOptionOrigin = localStorage.getItem(
+      "selectedOptionOrigin"
+    );
+    const lastSelectedOptionDestination = localStorage.getItem(
       "selectedOptionDestination"
-  );
-  const selectedDate = localStorage.getItem("selectedDate");
+    );
+    const selectedDate = localStorage.getItem("selectedDate");
 
-  var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-  const raw = JSON.stringify({
-    originId: lastSelectedOptionOrigin,
-    destinationId: lastSelectedOptionDestination,
-    departureDate: selectedDate,
-  });
-  console.log(raw);
+    const raw = JSON.stringify({
+      originId: lastSelectedOptionOrigin,
+      destinationId: lastSelectedOptionDestination,
+      departureDate: selectedDate,
+    });
 
-  const requestOptionsJourney = {
-    method: "POST",
-    body: raw,
-    redirect: "follow",
-    headers: myHeaders,
+    const requestOptionsJourney = {
+      method: "POST",
+      body: raw,
+      redirect: "follow",
+      headers: myHeaders,
+    };
+
+    fetch("http://localhost:3000/getJourney", requestOptionsJourney)
+      .then((response) => response.json())
+      .then((data) => {
+        let getLocationsOrigin;
+        let getLocationsDestination;
+        let journeyNewDate;
+
+        getLocationsOrigin = document.createElement("p");
+        getLocationsOrigin.innerHTML = data.data[0]["origin-location"];
+        locations.appendChild(getLocationsOrigin);
+
+        getLocationsDestination = document.createElement("p");
+        getLocationsDestination.innerHTML =
+          data.data[0]["destination-location"];
+        destinations.appendChild(getLocationsDestination);
+
+        journeyNewDate = document.createElement("p");
+        journeyNewDate.innerHTML = data.data[0].journey.departure
+          .split("T")[0]
+          .replaceAll("-", " / ");
+        journeyDate.appendChild(journeyNewDate);
+
+        for (let i = 0; i < data.data.length; i++) {
+          const newCurrency = data.data[i].journey.currency;
+          const internetPrice = data.data[i].journey["internet-price"];
+          const arrivalTime = data.data[i].journey.arrival
+            .split("T")[1]
+            .substring(0, 5);
+          const departureTime = data.data[i].journey.departure
+            .split("T")[1]
+            .substring(0, 5);
+          const journeyOrigin = data.data[i].journey.origin;
+          const journeyDestination = data.data[i].journey.destination;
+          const new_card = `  <div class="card mb-2">
+            <div class="row card-row">
+              <div class="col">
+                <div class="card-info">
+                  <div class="d-flex flex-column">
+                    <span>Kalkış</span>
+                    <span><span id="departureTime">${departureTime}</span></span>
+                  </div>
+                  <span class="d-flex justify-content-center mx-2">
+                      <?xml version="1.0" encoding="iso-8859-1"?>
+                      <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+                      <svg
+                        height="12"
+                        width="12"
+                        version="1.1"
+                        id="Layer_1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                        viewBox="0 0 483.563 483.563"
+                        xml:space="preserve"
+                      >
+                        <g>
+                          <polygon
+                            style="fill: #192289"
+                            points="483.563,221.781 148.61,221.781 148.61,93.171 0,241.781 148.61,241.781 148.61,261.781 
+                                483.563,261.781 	"
+                          />
+                          <polygon
+                            style="fill: #192289"
+                            points="0,241.781 148.61,390.392 148.61,241.781 	"
+                          />
+                        </g>
+                      </svg>
+                  </span>
+                  <div class="d-flex flex-column mx-2">
+                    <span>Varış</span>
+                    <span> <span id="arrivalTime">${arrivalTime}</span></span>
+                  </div>
+                </div>
+              
+              </div>
+  
+              <div class="col d-flex justify-content-end align-items-start">
+                <span class="card-price" >
+                  <span id="price">${internetPrice}</span>
+                  <span id="currency">${newCurrency}</span>
+                </span>
+              </div>
+            </div>
+            <div class="d-flex align-items-center">
+              <span id="journeyOrigin">${journeyOrigin}</span> 
+              <span class="mx-2"> - </span>
+              <span id="journeyDestination">${journeyDestination}</span>
+            </div>
+          </div>`;
+
+          cardWrapper[0].innerHTML += new_card;
+        }
+      })
+      .catch((error) => console.log("error", error));
   };
-
-  fetch("http://localhost:3000/getJourney", requestOptionsJourney)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      let journeyArea;
-      let destinationArea;
-      let journeyStartHour;
-      let journeyEndHour;
-      let journeyNewDate;
-      let internetPrice;
-      let newCurrency;
-      let getLocationsOrigin;
-
-      getLocationsOrigin = document.createElement("p");
-      getLocationsOrigin.innerHTML = data.data[0]["origin-location"];
-      locations.appendChild(getLocationsOrigin);
-
-      getLocationsDestination = document.createElement("p");
-      getLocationsDestination.innerHTML = data.data[0]["destination-location"];
-      destinations.appendChild(getLocationsDestination);
-
-      journeyArea = document.createElement("p");
-      journeyArea.innerHTML = data.data[0].journey.origin;
-
-      journeyOrigin.appendChild(journeyArea);
-
-      destinationArea = document.createElement("p");
-      destinationArea.innerHTML = data.data[0].journey.destination;
-
-      journeyDestination.appendChild(destinationArea);
-
-      journeyStartHour = document.createElement("p");
-      journeyStartHour.innerHTML = data.data[0].journey.departure
-        .split("T")[1]
-        .substring(0, 5);
-      departureTime.appendChild(journeyStartHour);
-
-      journeyNewDate = document.createElement("p");
-      journeyNewDate.innerHTML = data.data[0].journey.departure
-        .split("T")[0]
-        .replaceAll("-", " / ");
-      journeyDate.appendChild(journeyNewDate);
-
-      journeyEndHour = document.createElement("p");
-      journeyEndHour.innerHTML = data.data[0].journey.arrival
-        .split("T")[1]
-        .substring(0, 5);
-      arrivalTime.appendChild(journeyEndHour);
-
-      internetPrice = document.createElement("p");
-      internetPrice.innerHTML = data.data[0].journey["internet-price"];
-      price.appendChild(internetPrice);
-
-      newCurrency = document.createElement("p");
-      newCurrency.innerHTML = data.data[0].journey.currency;
-      currency.appendChild(newCurrency);
-    })
-    .catch((error) => console.log("error", error));
-};
-window.addEventListener("load", () => {
-  getJourneys();
-});
+  window.addEventListener("load", () => {
+    getJourneys();
+  });
 } catch (error) {
-console.log(error);
+  console.log(error);
 }
